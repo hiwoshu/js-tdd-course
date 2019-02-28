a93e9e0b4be0ff4678d32f3e2182de66cf88309b
Hiwoshu <hiwoshu@hotmail.com>
2019-02-28 10:06:04 -0300

    Add FizzBuzz

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-const a = 2;
-const b = 3;
-const b = '801';
-
-function sum() {
-  return a + b + c;
+const FizzBuzz = (num) => {
+  if (num === 0) return 0;
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
+  return num;
 }
 
-sum();
+export default FizzBuzz;
